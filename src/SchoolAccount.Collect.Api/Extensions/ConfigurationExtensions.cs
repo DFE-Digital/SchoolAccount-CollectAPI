47745864c65d416bcf0767ce9a41006101703965
markharrop <mark.harrop@education.gov.uk>
2026-08-13 08:42:22 +0100

    Add support for reading app confguration fom Azure
    Add extensions file

---
+++ src/SchoolAccount.Collect.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace SchoolAccount.Collect.Api.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static IConfigurationBuilder AddAzureAppConfiguration(
+        this IConfigurationBuilder configurationBuilder
+    )
+    {
+        IConfiguration configuration = configurationBuilder.Build();
+        string endpoint =
+            configuration["AzureAppConfiguration:Endpoint"]
+            ?? throw new InvalidOperationException(
+                "The setting `AzureAppConfiguration:Endpoint` was not found."
+            );
+
+        var credentials = new DefaultAzureCredential();
+
+        configurationBuilder.AddAzureAppConfiguration(options =>
+            options
+                .Connect(new Uri(endpoint), credentials)
+                .ConfigureKeyVault(kv => kv.SetCredential(credentials))
+        );
+
+        return configurationBuilder;
+    }
+}
