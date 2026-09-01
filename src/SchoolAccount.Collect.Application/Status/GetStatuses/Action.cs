19a0c8751de53e761d9c7521d8b7fd20e10b4c75
KianW-DfE <kian.winstanley@education.gov.uk>
2026-09-01 14:30:13 +0100

    Allow specific actions to be identified

--- src/SchoolAccount.Collect.Application/Status/GetStatuses/Action.cs
+++ src/SchoolAccount.Collect.Application/Status/GetStatuses/Action.cs
@@ -2,6 +2,7 @@ namespace SchoolAccount.Collect.Application.Status.GetStatuses;
 
 public class Action
 {
+    public string Id { get; init; }
     public string Name { get; init; }
     public Status Status { get; init; }
 }
