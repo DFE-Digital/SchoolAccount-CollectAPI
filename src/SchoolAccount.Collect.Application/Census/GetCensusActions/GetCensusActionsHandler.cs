36b80642a95f31371143b326d153b1d8ee5a7157
Paul Custance <paul.custance@education.gov.uk>
2026-09-04 14:46:19 +0100

    Add a config option to toggle db connectivity

--- src/SchoolAccount.Collect.Application/Census/GetCensusActions/GetCensusActionsHandler.cs
+++ src/SchoolAccount.Collect.Application/Census/GetCensusActions/GetCensusActionsHandler.cs
@@ -1,15 +1,27 @@
+using Microsoft.Extensions.Options;
 using SchoolAccount.Collect.Application.Abstractions.Messaging;
+using SchoolAccount.Collect.Application.Configuration;
 using SchoolAccount.Collect.SharedKernel;
 
 namespace SchoolAccount.Collect.Application.Census.GetCensusActions;
 
-public class GetCensusActionsHandler : IQueryHandler<GetCensusActionsQuery, CensusActionsResponse>
+public class GetCensusActionsHandler(IOptionsSnapshot<CensusSettings> settings)
+    : IQueryHandler<GetCensusActionsQuery, CensusActionsResponse>
 {
+    private readonly CensusSettings _settings = settings.Value;
+
     public async Task<Result<CensusActionsResponse>> Handle(
         GetCensusActionsQuery query,
         CancellationToken cancellationToken
     )
     {
+        if (_settings.UseDatabase)
+        {
+            throw new NotImplementedException(
+                "Reading census data from a database is not supported."
+            );
+        }
+
         CensusActionsResponse response = StubbedCensusResponse.Create();
 
         return await Task.FromResult(Result.Success(response));
--- src/SchoolAccount.Collect.Application/DependencyInjection.cs
+++ src/SchoolAccount.Collect.Application/DependencyInjection.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using SchoolAccount.Collect.Application.Abstractions.Behaviors;
 using SchoolAccount.Collect.Application.Abstractions.Messaging;
+using SchoolAccount.Collect.Application.Configuration;
 
 namespace SchoolAccount.Collect.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
     {
+        services.Configure<CensusSettings>(configuration.GetSection(CensusSettings.SectionName));
+
         services.Scan(scan =>
             scan.FromAssembliesOf(typeof(DependencyInjection))
                 .AddClasses(
