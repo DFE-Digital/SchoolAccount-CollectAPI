d877cd9cdf1cbd12f98f318193e9a69e46d08cec
KianW-DfE <kian.winstanley@education.gov.uk>
2026-09-14 10:39:37 +0100

    Improve test coverage for validation

---
+++ tests/SchoolAccount.Collect.Application.UnitTests/Configuration/CensusSettingsValidatorTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using SchoolAccount.Collect.Application.Configuration;
+using Shouldly;
+
+namespace SchoolAccount.Collect.Application.UnitTests.Configuration;
+
+public class CensusSettingsValidatorTests
+{
+    private readonly CensusSettingsValidator _sut = new();
+
+    [Fact]
+    public void Validate_returns_validation_problem_when_database_is_used_with_empty_connection_string()
+    {
+        // Act
+        ValidateOptionsResult result = _sut.Validate("name", CreateSettings(true, string.Empty));
+
+        // Assert
+        result.Failed.ShouldBeTrue();
+        result.FailureMessage.ShouldBe(
+            "The connection string must be provided when using the database."
+        );
+    }
+
+    private static CensusSettings CreateSettings(bool useDatabase, string? connectionString)
+    {
+        return new CensusSettings
+        {
+            UseDatabase = useDatabase,
+            ConnectionString = connectionString,
+        };
+    }
+}
