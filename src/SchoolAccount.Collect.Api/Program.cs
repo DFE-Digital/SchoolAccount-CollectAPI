c2f9b7a464341c28a4b725c4685d6c6a313dbd0a
Paul Custance <paul.custance@airelogic.com>
2026-09-16 23:07:26 +0100

    Load Azure App Config outside development

--- src/SchoolAccount.Collect.Api/Program.cs
+++ src/SchoolAccount.Collect.Api/Program.cs
@@ -14,7 +14,7 @@ builder.Host.UseSerilog(
     (context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration)
 );
 
-if (builder.Environment.IsProduction())
+if (!builder.Environment.IsDevelopment() && !builder.Environment.IsEnvironment("IntegrationTests"))
 {
     builder.Configuration.AddAzureAppConfiguration();
 }
