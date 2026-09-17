0756317819274711e3f98d1339b49d44afaa8506
markharrop <mark.harrop@education.gov.uk>
2026-09-17 13:28:45 +0100

    Use Title case consistently in status mapper

--- src/SchoolAccount.Collect.Application/Status/GetStatuses/ReturnStatusMapper.cs
+++ src/SchoolAccount.Collect.Application/Status/GetStatuses/ReturnStatusMapper.cs
@@ -6,31 +6,31 @@ public static class ReturnStatusMapper
     {
         { 1, "No Data" },
         { 23, "Awaiting File Upload" },
-        { 24, "File Upload in Progress" },
+        { 24, "File Upload In Progress" },
         { 31, "File Uploaded" },
         { 32, "File Upload Failed" },
-        { 13, "Waiting for validation" },
-        { 12, "Validation in progress" },
+        { 13, "Waiting For Validation" },
+        { 12, "Validation In Progress" },
         { 11, "Uploaded Validation Failed" },
-        { 2, "Loaded and Validated" },
-        { 3, "Amended by source" },
+        { 2, "Loaded And Validated" },
+        { 3, "Amended By Source" },
         { 14, "Awaiting Submission" },
-        { 17, "Submission in Progress" },
+        { 17, "Submission In Progress" },
         { 4, "Submitted" },
         { 5, "Rejected" },
-        { 6, "Amended by agent" },
+        { 6, "Amended By Agent" },
         { 15, "Awaiting Approval" },
-        { 18, "Approval in Progress" },
+        { 18, "Approval In Progress" },
         { 7, "Approved" },
-        { 8, "Rejected by collector" },
-        { 9, "Amended by collector" },
+        { 8, "Rejected By Collector" },
+        { 9, "Amended By Collector" },
         { 16, "Awaiting Authorisation" },
         { 19, "Authorisation In Progress" },
         { 10, "Authorised" },
         { 25, "Awaiting Matching" },
-        { 26, "Matching in Progress" },
+        { 26, "Matching In Progress" },
         { 27, "Awaiting Reconciliation" },
-        { 28, "Reconciliation in Progress" },
+        { 28, "Reconciliation In Progress" },
         { 29, "Matching Failed" },
         { 30, "Reconciliation Failed" },
     };
@@ -39,6 +39,6 @@ public static class ReturnStatusMapper
     {
         return StatusDescriptions.TryGetValue(statusCode, out string? description)
             ? description
-            : "Not started";
+            : "Not Started";
     }
 }
