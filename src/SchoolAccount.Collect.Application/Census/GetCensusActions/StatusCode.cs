6a5608a69b4fd12bcb636c21cc3d17c55235a0dc
Paul Custance <paul.custance@airelogic.com>
2026-09-17 22:14:50 +0100

    Remove unused code

--- src/SchoolAccount.Collect.Application/Census/GetCensusActions/StatusCode.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolAccount.Collect.Application.Census.GetCensusActions;
-
-public enum StatusCode
-{
-    Approved = 7,
-    AmendedByCollector = 9,
-    Authorised = 10,
-    AwaitingAuthorisation = 16,
-}
